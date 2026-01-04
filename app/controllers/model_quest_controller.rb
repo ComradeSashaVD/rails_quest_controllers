1c934ce2d2cfa7fafce11db0887f9bfcd9f43d40
Александр Владимирович Дудников <146486357+ComradeSashaVD@users.noreply.github.com>
2026-01-04 22:41:56 +0300

    Оптимизировал контроллеры
    Теперь количество обращений контроллеров к БД: O(1), вместо О(N)

--- app/controllers/model_quest_controller.rb
+++ app/controllers/model_quest_controller.rb
@@ -9,13 +9,18 @@ def step2
 end
 
 def step3
-  @enrollments = Enrollment.all
+  #@enrollments = Enrollment.all
+  @enrollments = Enrollment.includes(:student, :course).all
+  #Почему нужен .includes?
+  # - Потому что с помощью includes мы не к БД каждый раз обращаемся, а берём предзагружаем данные в хэш, и данном случае всего 3 запросами обходимся
 end
 
 def step4
-   @students = Student.all 
+  #@students = Student.all
+  @students = Student.includes(:grades).all
 end
 
+=begin
 def step5
   @courses_groups = Course.all.map do |course|
     {
@@ -28,7 +33,42 @@ def step5
   @courses_groups.sort_by! { |cg| [cg[:cnt], cg[:title]] }
 
 end
-  
+=end
+
+  def step5
+    sql = <<-SQL
+    SELECT 
+      courses.title,
+      -- COUNT считает только НЕ NULL значения enrollments.id
+      -- Если запись удовлетворяет условиям в LEFT JOIN, она учитывается
+      COUNT(enrollments.id) as cnt
+    FROM courses
+    --LEFT JOIN означает: "взять ВСЕ курсы, даже если у них нет записей в enrollments"
+    --JOIN (INNER JOIN) показал бы только курсы с записями    
+    LEFT JOIN enrollments ON enrollments.course_id = courses.id
+    --Берутся только записи enrollments, у которых:
+    --status IS NULL (никогда не менялся, значит активный)
+    --ИЛИ status = 'active' (явно указан как активный)
+      AND (enrollments.status IS NULL OR enrollments.status = 'active')
+    --Группировка по курсам - обязательна при использовании агрегатных функций (COUNT)
+    --Нужно указать ВСЕ неагрегированные поля из SELECT
+    GROUP BY courses.id, courses.title
+    ORDER BY cnt ASC, courses.title
+  SQL
+
+    # Выполнение "сырого" SQL запроса через ActiveRecord
+    result = ActiveRecord::Base.connection.execute(sql)
+
+    # Преобразование результата SQL запроса в удобный для Rails формат
+    @courses_groups = result.map do |row|
+      # Для каждой строки создаем хэш с ключами:
+      # :title - название курса (уже в правильном регистре из БД)
+      # :cnt - количество активных записей, преобразованное в целое число
+      # .to_i нужно потому что из БД значение приходит как строка
+      { title: row['title'], cnt: row['cnt'].to_i }
+    end
+  end
+
  def final
  
   end 
